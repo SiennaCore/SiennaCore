5d6ed7ffe50c89a580d58c73f94a3e82da5801b0
Kaldorei <Kaldorei@314f1d10-98f6-48e5-a4d7-766c1cb0bb34>
2011-02-01 10:59:35 +0000

    The launcher now handle custom Certificate Server
    git-svn-id: http://subversion.assembla.com/svn/sienna-rift@8 314f1d10-98f6-48e5-a4d7-766c1cb0bb34

--- Tools/Launcher/Form1.cs
+++ Tools/Launcher/Form1.cs
@@ -10,10 +10,24 @@ using System.Windows.Forms;
 using System.IO;
 using System.Diagnostics;
 
+using System.Security.Cryptography;
+
+using System.Net;
+using System.Web;
+
 namespace Launcher
 {
     public partial class Form1 : Form
     {
+        /// 
+        /// CONFIG
+        /// 
+
+        private bool UsingCertificateServer = false;
+        private string CertificateServerUrl = "http://127.0.0.1/GetCertificate.php";
+
+        //////////////////////////
+
         public Form1()
         {
             InitializeComponent();
@@ -23,30 +37,84 @@ namespace Launcher
         {
             string Username = textBox1.Text;
             string Password = textBox2.Text;
+
+            bool CanContinue = false;
+
+            SHA1CryptoServiceProvider Crypto = new SHA1CryptoServiceProvider();
+            Crypto.Initialize();
+            
+            byte[] CryptPass = Crypto.TransformFinalBlock(Encoding.UTF8.GetBytes(Password), 0, Encoding.UTF8.GetByteCount(Password));
+            Password = Encoding.UTF8.GetString(CryptPass);
+
             string Lang = listBox1.SelectedItem != null ? (string)listBox1.SelectedItem : "English";
 
             string RemoteServer = textBox3.Text;
 
             string cert = "";
 
-            if(!File.Exists(cert + @"\SiennaCert.pfx"))
-                File.Create(cert + @"\SiennaCert.pfx");
-
-            using (StreamWriter writer = new StreamWriter(cert + @"\SiennaCert.pfx"))
+            if (!UsingCertificateServer)
             {
-                writer.WriteLine("Signature: SiennaAuth");
-                writer.WriteLine("<?xml version=\"1.0\"?>");
-                writer.WriteLine("<ClientAuthCertificate xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">");
-                writer.WriteLine("<Username>" + Username + "</Username>");
-                writer.WriteLine("<Hash>" + Password + "</Hash>");
-                writer.WriteLine("<Sessionkey></Sessionkey>");
-                writer.WriteLine("</ClientAuthCertificate>");
+                using (StreamWriter writer = new StreamWriter(cert + @"\SiennaCert.pfx"))
+                {
+                    writer.WriteLine("Signature: SiennaAuth");
+                    writer.WriteLine("<?xml version=\"1.0\"?>");
+                    writer.WriteLine("<ClientAuthCertificate xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">");
+                    writer.WriteLine("<Username>" + Username + "</Username>");
+                    writer.WriteLine("<Hash>" + Password + "</Hash>");
+                    writer.WriteLine("<Sessionkey></Sessionkey>");
+                    writer.WriteLine("</ClientAuthCertificate>");
+                }
+
+                CanContinue = true;
+            }
+            else
+            {
+                WebClient wc = new WebClient();
+                string Certificate = wc.DownloadString(CertificateServerUrl + "?username=" + Username + "&passhash=" + Password);
+
+                switch (Certificate)
+                {
+                    case "ERR_INVALID_USERNAME":
+                        MessageBox.Show("Error, your username is invalid");
+                        break;
+
+                    case "ERR_INVALID_PASSWORD":
+                        MessageBox.Show("Error, your password is invalid");
+                        break;
+
+                    case "ERR_ACTIVE_BAN":
+                        MessageBox.Show("Error, you are banned from the server");
+                        break;
+
+                    default:
+                        CanContinue = true;
+                        break;
+                }
+
+                if (CanContinue)
+                {
+                    using (StreamWriter writer = new StreamWriter(cert + @"\SiennaCert.pfx"))
+                    {
+                        writer.WriteLine("Signature: SiennaAuth");
+                        writer.Write(Certificate);
+                    }
+                }
             }
 
-            Process process = new Process();
-            process.StartInfo.FileName = "rift.exe";
-            process.StartInfo.Arguments = "-u " + Username + " -k " + cert + @"\SiennaCert.pfx -l " + Lang + " -s " + RemoteServer;
-            process.Start();
+            if (CanContinue)
+            {
+                try
+                {
+                    Process process = new Process();
+                    process.StartInfo.FileName = "rift.exe";
+                    process.StartInfo.Arguments = "-u " + Username + " -k " + cert + @"\SiennaCert.pfx -l " + Lang + " -s " + RemoteServer;
+                    process.Start();
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error, the launcher must be placed in the game directory");
+                }
+            }
         }
     }
 }
