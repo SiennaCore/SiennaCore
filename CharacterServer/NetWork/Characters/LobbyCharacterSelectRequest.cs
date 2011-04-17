731b6ca9b77a23a97f4ed48dd3315d92bdfa318b
dyox <dyox@314f1d10-98f6-48e5-a4d7-766c1cb0bb34>
2011-04-17 13:55:12 +0000

    ** Missing File
    git-svn-id: http://subversion.assembla.com/svn/sienna-rift@56 314f1d10-98f6-48e5-a4d7-766c1cb0bb34

---
+++ CharacterServer/NetWork/Characters/LobbyCharacterSelectRequest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
+
+using Shared;
+using Shared.Database;
+using Shared.NetWork;
+
+namespace CharacterServer
+{
+    [ISerializableAttribute((long)Opcodes.LobbyCharacterSelectResponse)]
+    public class LobbyCharacterSelectResponse : ISerializablePacket
+    {
+        [ListBit(1)]
+        public List<string> Ips = new List<string>();
+    }
+
+    [ISerializableAttribute((long)Opcodes.LobbyCharacterSelectRequest)]
+    public class LobbyCharacterSelectRequest : ISerializablePacket
+    {
+        [Unsigned7Bit(0)]
+        public long Field0;
+
+        public override void OnRead(RiftClient From)
+        {
+            Log.Success("SelectRequest","Enter on World : " + From.GetIp + ",Field="+Field0);
+
+            LobbyCharacterSelectResponse Rp = new LobbyCharacterSelectResponse();
+            if (From.Realm != null)
+            {
+                Log.Success("Entering", "On : " + From.Realm.Address);
+                Rp.Ips.Add(From.Realm.Address);
+            }
+
+            From.SendSerialized(Rp);
+        }
+    }
+}
