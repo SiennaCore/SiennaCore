948f88d4e74b795d1c764b301973f1db333c1375
Zelos <Zelos@314f1d10-98f6-48e5-a4d7-766c1cb0bb34>
2011-04-27 13:24:13 +0000

    RealmID fix.
    git-svn-id: http://subversion.assembla.com/svn/sienna-rift@65 314f1d10-98f6-48e5-a4d7-766c1cb0bb34

--- Shared/Database/Characters/Realm.cs
+++ Shared/Database/Characters/Realm.cs
@@ -53,98 +53,172 @@ public class Realm : DataObject
         switch (RealmId)
         {
             case 1:
-                Name = "Mordant";
-                RiftId = 2571;
+                Name = "Imperium";
+                RiftId = 2564;
                 return; ;
             case 2:
+                Name = "Scarhide";
+                RiftId = 2562;
+                return;
+            case 3:
+                Name = "heatherfield";
+                RiftId = 2583;
+                return;
+            case 4:
+                Name = "Tahkaat";
+                RiftId = 2582;
+                return;
+            case 5:
+                Name = "Cinderon";
+                RiftId = 2584;
+                return;
+            case 6:
+                Name = "maidenfalls";
+                RiftId = 2572;
+                return;
+            case 7:
+                Name = "Sparkwing";
+                RiftId = 2563;
+                return;
+            case 8:
+                Name = "Sagespire";
+                RiftId = 2504;
+                return; ;
+            case 9:
+                Name = "Firesand";
+                RiftId = 2522;
+                return;
+            case 10:
+                Name = "Feenring";
+                RiftId = 2542;
+                return;
+            case 11:
                 Name = "Refuge";
                 RiftId = 2591;
                 return;
-            case 3:
-                Name = "Quarrystone";
-                RiftId = 2574;
+            case 12:
+                Name = "Mordant";
+                RiftId = 2571;
                 return;
-            case 4:
-                Name = "Heaterfield";
-                RiftId = 2583;
+            case 13:
+                Name = "Overlook";
+                RiftId = 2561;
                 return;
-            case 5:
-                Name = "Maidenfalls";
-                RiftId = 2572;
-                return;
-
-            case 6:
-                Name = "Riptalon";
-                RiftId = 2514;
-                return;
-            case 7:
+            case 14:
                 Name = "Icewatch";
                 RiftId = 2593;
                 return;
-
-            case 8:
-                Name = "Rubicon";
-                RiftId = 2582;
-                return; ;
-            case 9:
-                Name = "Brutwatch";
-                RiftId = 55057;
-                return;
-            case 10:
-                Name = "Trûbkopf";
-                RiftId = 55313;
-                return;
-            case 11:
-                Name = "Argent";
-                RiftId = 57105;
-                return;
-            case 12:
-                Name = "Firesand";
-                RiftId = 57361;
-                return;
-            case 13:
-                Name = "Sagespire";
-                RiftId = 57617;
-                return;
-            case 14:
-                Name = "Illumintation";
-                RiftId = 57873;
-                return;
             case 15:
-                Name = "Tempête";
-                RiftId = 59665;
+                Name = "Riptalon";
+                RiftId = 2514;
                 return;
             case 16:
-                Name = "Brisesol";
-                RiftId = 59921;
+                Name = "Quarrystone";
+                RiftId = 2574;
                 return;
             case 17:
-                Name = "Akala";
-                RiftId = 60177;
+                Name = "Bloodiron";
+                RiftId = 2521;
                 return;
             case 18:
-                Name = "Feenring";
-                RiftId = 60433;
+                Name = "Cloudborne";
+                RiftId = 2502;
                 return;
             case 19:
-                Name = "Poudre-granite";
-                RiftId = 60178;
+                Name = "Shivermere";
+                RiftId = 2512;
                 return;
             case 20:
-                Name = "Tempêtueux";
-                RiftId = 45330;
+                Name = "Zareph";
+                RiftId = 2613;
                 return;
             case 21:
-                Name = "Sprosspassage";
-                RiftId = 47634;
+                Name = "Phynnious";
+                RiftId = 2632;
                 return;
             case 22:
-                Name = "Kristaltiefiend";
-                RiftId = 47890;
+                Name = "Steampike";
+                RiftId = 2503;
+                return;
+			case 23:
+                Name = "Icewatch";
+                RiftId = 2511;
+                return;
+            case 24:
+                Name = "Rhazade";
+                RiftId = 2533;
+                return;
+            case 25:
+                Name = "Argent";
+                RiftId = 2513;
+                return;
+            case 26:
+                Name = "Centius";
+                RiftId = 2631;
+                return;
+            case 27:
+                Name = "Felsspitze";
+                RiftId = 2603;
+                return;
+            case 28:
+                Name = "Blightweald";
+                RiftId = 2501;
+                return;
+            case 29:
+                Name = "Grimnir";
+                RiftId = 2634;
+                return;
+            case 30:
+                Name = "Tempête";
+                RiftId = 2554;
+                return;
+            case 31:
+                Name = "Trübkopf";
+                RiftId = 2532;
+                return;
+            case 32:
+                Name = "Spross-Passage";
+                RiftId = 2534;
+                return;
+            case 33:
+                Name = "Akala";
+                RiftId = 2544;
+                return;
+            case 34:
+                Name = "Cestus";
+                RiftId = 2553;
+                return;
+            case 35:
+                Name = "Rubicon";
+                RiftId = 2552;
+                return;
+            case 36:
+                Name = "Granitstaub";
+                RiftId = 2611;
+                return;
+            case 37:
+                Name = "Brutwacht";
+                RiftId = 2531;
+                return;
+            case 38:
+                Name = "Brutmutter";
+                RiftId = 2601;
+                return;
+            case 39:
+                Name = "Immerwacht";
+                RiftId = 2541;
+                return;
+            case 40:
+                Name = "Brisesol";
+                RiftId = 2551;
+                return;
+            case 41:
+                Name = "Whitefall";
+                RiftId = 2523;
                 return;
 
             default:
-                Name = "Custom";
+                Name = "SiennaCore custom";
                 RiftId = RealmId;
                 break;
         };
