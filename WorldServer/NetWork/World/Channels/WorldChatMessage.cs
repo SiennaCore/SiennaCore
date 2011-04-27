f391d1e18b631bd32e9c61cc7b954e9aa5bd8ec7
Valkyr <Valkyr@314f1d10-98f6-48e5-a4d7-766c1cb0bb34>
2011-04-27 16:35:48 +0000

    - Missing files
    git-svn-id: http://subversion.assembla.com/svn/sienna-rift@68 314f1d10-98f6-48e5-a4d7-766c1cb0bb34

---
+++ WorldServer/NetWork/World/Channels/WorldChatMessage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Shared;
+
+namespace WorldServer
+{
+    [ISerializableAttribute((long)Opcodes.WorldChatMessage)]
+    public class WorldChatMessage : ISerializablePacket
+    {
+        [ArrayBit(0)]
+        public string ChannelName;
+
+        [ArrayBit(3)]
+        public string Message;
+
+        public override void OnRead(RiftClient From)
+        {
+            if (Message.StartsWith(".teleport"))
+            {
+                string CmdArgs = Message.Substring(10);
+                string[] pos = CmdArgs.Split(new char[] { ' ' });
+
+                WorldServerPositionUpdate WPos = new WorldServerPositionUpdate();
+
+                WPos.GUID = 123456;
+
+                WPos.Position = new List<float>();
+                WPos.Position.Add(float.Parse(pos[0]));
+                WPos.Position.Add(float.Parse(pos[1]));
+                WPos.Position.Add(float.Parse(pos[2]));
+
+                WPos.Orientation = new List<float>();
+                WPos.Orientation.Add(0.0f);
+                WPos.Orientation.Add(0.0f);
+                WPos.Orientation.Add(0.0f);
+
+                From.SendSerialized(WPos);
+            }
+        }
+    }
+}
---
+++ WorldServer/NetWork/World/Movements/WorldServerPositionUpdate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Shared;
+
+namespace WorldServer
+{
+    [ISerializableAttribute((long)Opcodes.WorldServerPositionUpdate)]
+    public class WorldServerPositionUpdate : ISerializablePacket
+    {
+        [Raw8Bit(0)]
+        public long GUID;
+
+        [ListBit(1)]
+        public List<float> Position;
+
+        [Unsigned7Bit(2)]
+        public long UnkMovementInfo1 = 10569;
+
+        [Unsigned7Bit(3)]
+        public long UnkMovementInfo2 = 32767;
+
+        [ListBit(4)]
+        public List<float> Orientation;
+
+        [BoolBit(12)]
+        public bool UnkMovementInfo3;
+
+        public override void OnRead(RiftClient From)
+        {
+            Log.Info("Movement Info", "Received client movement information");
+        }
+    }
+}
